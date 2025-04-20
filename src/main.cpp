62c2a980d536197d5810047490da1240af76f6a5
Fryy55 <ivan_ugu55@icloud.com>
2025-04-21 01:38:39 +0500

    v1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
 #include <Geode/Geode.hpp>
+#include <Geode/utils/terminate.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
+class $modify(MenuLayer) {
+	$override bool init() {
+		if (!MenuLayer::init())
+			return false;
+
+		auto popup = createQuickPopup(
+			"phantom.",
+			"<cr>Do you hate it?</c>",
+			"Yes.", "No.",
+			[](bool yes, bool no) {
+				if (yes)
+					createQuickPopup(
+						"phantom...",
+						"<cr>UNINSTALL?</c>",
+						"Yes.", "Yes.",
+						[](auto, auto) {
+							static_cast<void>(Mod::get() -> uninstall(true));
+							utils::game::restart();
+						}
+					);
+				if (no)
+					utils::terminate("fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you");
+			},
+			false
+		);
+		popup -> m_scene = this;
+		popup -> show();
+
+		return true;
+	}
+};
